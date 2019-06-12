f479cc2e6551e4a60b6ce447d88295fa78a274ac
Greg Hsu <greghsu23@gmail.com>
2019-06-12 12:04:02 -0400

    create schema

---
+++ models/conventions.js
@@ -0,0 +1,20 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+
+var skillSchema = Schema( {
+
+  ConventionName: String,
+  ConventionLocation: String,
+  ConventionDate: String,
+  ConventionType: String,
+  ConventionDescription: String,
+  ConventionTicketPrice: String,
+  ConventionCelebritiesGoing: String,
+  ConventionContactInformation: String,
+  ConventionPicture: String,
+  ConventionEvents: String
+
+} );
+
+module.exports = mongoose.model( 'Skill', skillSchema );
