00f5d89cd218c2c1392e3939e5fea2a57183ba6c
Daniel Monroy <dmonroy@brandeis.edu>
2019-07-05 14:55:03 -0400

    more debg

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 //var userSchema = mongoose.Schema( {any:{}})
 
 var userSchema = Schema( {
-  googleid: String,
+  googleid: String, 
   googletoken: String,
   googlename:String,
   googleemail:String,
