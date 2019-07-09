7b2e3d8429784236c799f7a34f2a403655b72359
Taylor Simala <tlsimala@brandeis.edu>
2019-07-09 12:47:43 -0400

    Changed location of ID and Secret
    Debugging

--- controllers/listController.js
+++ controllers/listController.js
@@ -2,8 +2,8 @@
 const Convention = require( '../models/Convention' );
 var Amadeus = require('amadeus');
 var amadeus = new Amadeus({
-    clientId: '2frveXyrGqBQ6GqkAahbIehhyIYnBWp6',
-    clientSecret: 'qXTvGJkep85jvMqm'
+    clientId: '../config/apitravel.js',
+    clientSecret: '../config/apisecret.js'
   });
 var amadeus = new Amadeus();
 
