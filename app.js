e6aacb1a9f959f94195b2c189ddcf46344d3d542
Ling <cathyxie@brandeis.edu>
2019-07-16 10:42:09 -0400

    testing
    aa

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ var ownerList= [
 
 app.get('/login/authorized',
         passport.authenticate('google', {
-                successRedirect : '/discussion',
+                successRedirect : '/',
                 failureRedirect : '/loginerror'
         })
       );
