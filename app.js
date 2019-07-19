ca32ff2035c7202e7f40fd1d608b0a4778c73012
Greg Hsu <greghsu23@gmail.com>
2019-07-19 10:12:22 -0400

    update app.js

--- app.js
+++ app.js
@@ -23,10 +23,9 @@ const localMongo = 'mongodb://localhost/convengo';
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    localMongo
-    //'mongodb://localhost/convengo' ||
-    //'mongodb://heroku_lzp0htxz:74m9me91evl2nmqh6qi0bn4t2b@ds247637.mlab.com:47637/heroku_lzp0htxz' ||
-    //'mongodb://heroku_03g7jdqb:hnnbgerrljnmvdlu2uc57sqt3t@ds243607.mlab.com:43607/heroku_03g7jdqb';
+    //localMongo ||
+    mlab
+    
 console.log("setting uristring to "+uristring)
 //uristring ='mongodb://localhost/convengo'
     // Makes connection asynchronously.  Mongoose will queue up database
